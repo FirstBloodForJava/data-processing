d6dd9a3fb8c5b5e052c257954b6f52b6c5d22d24
ouyangcm <mingorg@163.com>
2025-09-19 15:55:20 +0800

    xxl-job-executor 执行器初始化

---
+++ dp-xxl-job-executor/src/main/java/com/oycm/XxlJobExecutorApplication.java
@@ -0,0 +1,20 @@
+package com.oycm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ouyangcm
+ * create 2025/9/19 15:52
+ */
+@SpringBootApplication
+public class XxlJobExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobExecutorApplication.class, args);
+        while (true) {
+
+        }
+    }
+
+}
