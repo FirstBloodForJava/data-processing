1cb980e5472c6b9423b3e90ace97309b5e98ae98
ouyangcm <mingorg@163.com>
2025-09-25 20:32:04 +0800

    spark 任务枚举类

---
+++ dp-xxl-job-executor/src/main/java/com/oycm/enums/SparkJobType.java
@@ -0,0 +1,45 @@
+package com.oycm.enums;
+
+/**
+ * Spark 任务枚举类
+ */
+public enum SparkJobType {
+
+    SimpleSqlETL("SimpleSqlETL", "libs/sparkProgram/ap-spark.jar", "com.oycm.etl.v1.SimpleSqlEtlJob"),
+
+    ;
+
+    /**
+     * 配置的 spark 任务类型
+     */
+    private String type;
+
+    /**
+     * 该任务类型所在的 jar 位置
+     */
+    private String jarPath;
+
+    /**
+     * 该任务类型对应的启动类
+     */
+    private String clazz;
+
+    SparkJobType (String type, String jarPath, String clazz) {
+        this.type = type;
+        this.jarPath = jarPath;
+        this.clazz = clazz;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getJarPath() {
+        return jarPath;
+    }
+
+    public String getClazz() {
+        return clazz;
+    }
+
+}
