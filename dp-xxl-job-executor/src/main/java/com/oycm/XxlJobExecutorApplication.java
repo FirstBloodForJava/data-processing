429a2505ed301ea8f3cf2534badd587126f4deee
ouyangcm <mingorg@163.com>
2025-09-29 13:28:54 +0800

    循环调整

--- dp-xxl-job-executor/src/main/java/com/oycm/XxlJobExecutorApplication.java
+++ dp-xxl-job-executor/src/main/java/com/oycm/XxlJobExecutorApplication.java
@@ -1,22 +1,21 @@
 package com.oycm;
 
-import com.oycm.autoconfigure.SparkJobProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author ouyangcm
  * create 2025/9/19 15:52
  */
 @SpringBootApplication
-@EnableConfigurationProperties(SparkJobProperties.class)
 public class XxlJobExecutorApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(XxlJobExecutorApplication.class, args);
         while (true) {
-
+            TimeUnit.MINUTES.sleep(60);
         }
     }
 
